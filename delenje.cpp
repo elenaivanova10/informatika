8e7697f2af5b012785ac7f88d174b43387db4e2d
Elena Ivanova <elenaivanovagoldy@gmail.com>
2022-05-04 21:21:17 +0100

    vezba1

---
+++ delenje.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int broj1, broj2;
+    int kolicnik;
+    cout << "Vnesi go brojot 1\n";
+    cin >> broj1;
+    cout << "Vnesi go brojot 2\n";
+    cin >> broj2;
+    kolicnik = broj1 * broj2;
+    cout << kolicnik;
+
+    return 0;
+}
