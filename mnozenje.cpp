b9487b0c4dd2381351c7a4ccb4b31dcaa6dea8f4
Elena Ivanova <elenaivanovagoldy@gmail.com>
2022-05-04 21:16:43 +0100

    Domasno 2

---
+++ mnozenje.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int broj1, broj2;
+    int proizvod;
+    cout << "Vnesi go brojot 1\n";
+    cin >> broj1;
+    cout << "Vnesi go brojot 2\n";
+    cin >> broj2;
+    proizvod = broj1 * broj2;
+    cout << proizvod;
+
+    return 0;
+}
